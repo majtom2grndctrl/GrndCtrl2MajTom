b705fb43d0c1a73d05c5cb09d675d92e5ec4eef3
Dan Hiester <dhiester@distantlyyours.com>
2013-10-09 20:51:29 -0400

    Repaired basic sidebar navigation. :dizzy:

--- app/assets/javascripts/gcmt.js
+++ app/assets/javascripts/gcmt.js
@@ -1,18 +1,16 @@
-$(function(){
 
 // Hide JS required warnings
   $('#gcmt-JsRequired').hide(0);
 
   var gcmt = {};
-  gcmt.manage = {};
 
-  var enableBlogpostSubmit = function(postId) {
-    $("#gcmtMainWorkspace").submit(function(event){
+  gcmt.enableBlogpostSubmit = function( postId ) {
+    $("#gcmtMainWorkspace").submit( function( event ) {
       event.preventDefault();
-      alert("Submit intercepted");
-      var dataObject = new FormData(document.getElementById("gcmtMainWorkspace"));
+      var form = $("#gcmtMainWorkspace");
+      var dataObject = new FormData( form );
       var urlString;
-      if ( postId === 0 ) {
+      if ( postId == 0 ) {
         urlString = "new";
       } else {
         urlString = postId;
@@ -27,62 +25,53 @@ $(function(){
         beforeSend: preSubmitPost(),
         success: postSuccess(postId)
       });
-      return false;
+//      return false;
+    });
+  };
+
+// Load Nav Bar
+  gcmt.loadNavBar = function(page) {
+    $("#gcmtMainSidebarNav").load("/manage/blogPosts/list/" + page, function(response, status, xhr) {
+      if (status == "error") {
+        var msg = "Oops, there was an error: ";
+        alert(msg + xhr.status + " " + xhr.statusText)
+      } else {
+        gcmt.enableLeftNav();
+      }
     });
   };
 
 // Make post listings clickable
-  var enableLeftNav = function() {
+  gcmt.enableLeftNav = function() {
     $('#gcmtMainSidebarNav').children().click(function() {
       var postId = $(this).attr('data-post-id');
-      $(".gcmtMainWorkspace").addClass("gcmtUiDestroy");
 
       $(".gcmtMainWorkspaceContainer").queue( function() {
+        $(".gcmtMainWorkspace").addClass("gcmtUiDestroy");
+        $(this).delay(500);
         $(this).empty();
-        $(this).delay(1400).load('/manage/blogPosts/' + postId, function(response, status, xhr){
+        $(this).delay(1400).load('/manage/blogPosts/' + postId, function(response, status, xhr) {
           if (status == "success") {
             $(".gcmtMainWorkspace").addClass("gcmtUiInit");
           };
         });
-        enableBlogpostSubmit(postId);
+        gcmt.enableBlogpostSubmit(postId);
         $(this).dequeue();
       });
     });
   };
 
-// Load Nav Bar
-  var loadNavBar = function(page) {
-    $("#gcmtMainSidebarNav").load("/manage/blogPosts/list/" + page, function(response, status, xhr) {
-      if (status == "error") {
-        var msg = "Oops, there was an error: ";
-        alert(msg + xhr.status + " " + xhr.statusText)
-      }
-    });
-    enableLeftNav();
-  }
-
 // Submit the blog post
-  var preSubmitPost = function(){
+  gcmt.preSubmitPost = function(){
     $("#gcmtMainWorkspace, #gcmtMainSidebarNav").queue(function() {
       $(this).addClass("gcmtMainWorkspaceWait").delay(1000).dequeue();
     });
   }
 
-  var postSuccess = function(id) {
+  gcmt.postSuccess = function(id) {
     $("#gcmtMainSidebarNav").queue(function(){
       $(this).unload();
       $(this).load("/manage/blogPosts/list/" + id);
       $(this).dequeue();
     });
   };
-
-// Initialize the page
-  loadNavBar(0);
-  enableBlogpostSubmit(0);
-//  loadBlogpostsJson(0);
-  $("#gcmtMainWorkspace").queue(function(){
-    $(this).delay(500);
-    $(this).addClass("gcmtUiInit");
-    $(this).dequeue();
-  });
-});
