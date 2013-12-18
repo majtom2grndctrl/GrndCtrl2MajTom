90f622ec4453a24e3f81ddfde6545553055c6ba6
Dan Hiester <dhiester@distantlyyours.com>
2013-12-18 13:40:27 -0500

    Let's not kid ourselves. No one's ever going to use this.

--- app/models/SitePrefs.scala
+++ app/models/SitePrefs.scala
@@ -1,7 +1,8 @@
 package models
 
 object SitePrefs {
-  val name: String = "Your New Site"
-  val description = Some("Write a description of your site here to use as a default for pages that lack a description of their own")
-  val keywords = Some("keywords")
+  val name: String = "Distantly Yours"
+  val description = Some("The creative laboratory of Dan Hiester - web development, user experience design, creativity, and technology goodness from Bloomington, IN")
+  val keywords = Some("design, web design, user experience design, web development, html, css, creativity, technology, Bloomington, Indiana")
+  val copyrightOwner: String = "Dan Hiester"
 }
