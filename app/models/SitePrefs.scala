19f12d5b9b8ca5984d0e7b3c7d583be1f389fc1d
Dan Hiester <dhiester@distantlyyours.com>
2013-12-23 13:16:07 -0500

    Sync source from laptop.

--- app/models/SitePrefs.scala
+++ app/models/SitePrefs.scala
@@ -2,6 +2,7 @@ package models
 
 object SitePrefs {
   val name: String = "Distantly Yours"
+  val tagline: String = "The creative studio of Dan Hiester"
   val description = Some("The creative laboratory of Dan Hiester - web development, user experience design, creativity, and technology goodness from Bloomington, IN")
   val keywords = Some("design, web design, user experience design, web development, html, css, creativity, technology, Bloomington, Indiana")
   val copyrightOwner: String = "Dan Hiester"
